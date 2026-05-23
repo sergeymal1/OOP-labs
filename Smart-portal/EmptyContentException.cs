4ace23afe0da8a8e8914826fb982ea36150feedb
Sergeymal1 <sergejmalenkij@gmail.com>
2026-05-23 23:17:11 +0300

    feat: added custom exception EmptyContentException

---
+++ Smart-portal/EmptyContentException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmartPortal.Core.Exceptions
+{
+    // Виняток — порожній текст звернення
+    public class EmptyContentException : Exception
+    {
+        public EmptyContentException()
+            : base("Текст звернення не може бути порожнім")
+        {
+        }
+    }
+}
