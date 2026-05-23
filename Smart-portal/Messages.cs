d830db830b49409c4e37b07818186467de6c270a
Sergeymal1 <sergejmalenkij@gmail.com>
2026-05-23 21:41:18 +0300

    feat: added Messages file with all UI strings

---
+++ Smart-portal/Messages.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartPortal.Core
+{
+    // Файл із текстовими повідомленнями
+    public static class Messages
+    {
+        // Привітання
+        public const string WelcomeMessage = "Вітаємо на порталі міста";
+        public const string GoodMorning = "Добрий день";
+        public const string Goodbye = "До побачення";
+
+        // Помилки
+        public const string CitizenNotFound = "Громадянина з таким ID не знайдено";
+        public const string AppealNotFound = "Звернення з таким номером не знайдено";
+        public const string NotLoggedIn = "Спочатку увійдіть у систему";
+        public const string EmptyContent = "Текст звернення не може бути порожнім";
+        public const string UnknownCommand = "Невідома команда";
+
+        // Інформація
+        public const string AppealCreated = "Ваше звернення зареєстровано під номером";
+        public const string NoAppeals = "У вас поки немає звернень";
+        public const string WaitResponse = "Очікуйте на відповідь";
+
+        // Статуси
+        public const string StatusNew = "нове";
+        public const string StatusInProgress = "в роботі";
+        public const string StatusResolved = "вирішено";
+        public const string StatusRejected = "відхилено";
+    }
+}
