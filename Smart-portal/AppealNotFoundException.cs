7f765059e9a9715f6c7d4ec18ef024c74d0d505e
Sergeymal1 <sergejmalenkij@gmail.com>
2026-05-23 23:10:33 +0300

    feat: added custom exception AppealNotFoundException

---
+++ Smart-portal/AppealNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmartPortal.Core.Exceptions
+{
+    // Звернення не знайдено
+    public class AppealNotFoundException : Exception
+    {
+        public string AppealId { get; private set; }
+
+        public AppealNotFoundException(string appealId)
+            : base($"Звернення з номером '{appealId}' не знайдено")
+        {
+            AppealId = appealId;
+        }
+    }
+}
