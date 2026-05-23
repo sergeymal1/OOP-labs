46a50c6ecf3a45d23e6b968be544624a8eecb2a8
Sergeymal1 <sergejmalenkij@gmail.com>
2026-05-23 21:19:16 +0300

    feat: added abstract class BaseEntity

---
+++ Smart-portal/BaseEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SmartPortal.Core
+{
+    public abstract class BaseEntity
+    {
+        private string id;
+        private DateTime createdDate;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime CreatedDate
+        {
+            get { return createdDate; }
+            set { createdDate = value; }
+        }
+
+        // Конструктор за замовчуванням
+        public BaseEntity()
+        {
+            this.id = Guid.NewGuid().ToString().Substring(0, 8);
+            this.createdDate = DateTime.Now;
+        }
+
+        // Конструктор із параметрами
+        public BaseEntity(string id)
+        {
+            this.id = id;
+            this.createdDate = DateTime.Now;
+        }
+
+        // Кожен спадкоємець зобов'язаний реалізувати
+        public abstract string GetDisplayInfo();
+
+        public virtual bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(id);
+        }
+    }
+}
